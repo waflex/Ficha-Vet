5d1d3fec3f1626d1ede5a5d82d611a07d325faf7
Jonathan Rojas <wafle48@gmail.com>
2024-02-26 12:26:50 -0300

    Agregado rutas consultas

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
-import express from "express";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js";
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
+import Consultas from './routes/consultas.routes.js';
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", authRoutes);
+app.use('/api', authRoutes);
+app.use('/api', Consultas);
 
 export default app;
