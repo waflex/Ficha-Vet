0f655cd5c2685fb583904175af99e445733b3165
gatunah <144285026+gatunah@users.noreply.github.com>
2024-02-28 18:01:22 -0300

    Corecciones
    Se adapta formulario siguiendo la forma mobile first

--- client/src/pages/IngresoPage.jsx
+++ client/src/pages/IngresoPage.jsx
@@ -9,57 +9,61 @@ function IngresoPage() {
       console.log(values);
       const res = await registerRequest(values);
       console.log(res);
-    })} className="max-w-screen-lg mx-auto mt-8 p-8 border border-gray-300 rounded grid grid-cols-1 md:grid-cols-2 gap-8">
+    })} className="max-w-screen-lg mx-auto mt-8 p-4 border border-gray-300 rounded">
 
-      <div className="col-span-2 text-center mb-8">
-        <h1 className="text-4xl font-bold">Formulario de ingreso</h1>
+      <div className="text-center mb-4">
+        <h1 className="text-2xl font-bold">Formulario de ingreso</h1>
       </div>
 
-      <div>
-        <h2 className="text-xl font-bold mb-2">Datos tutor</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div>
+          <h2 className="text-xl font-bold mb-2">Datos tutor</h2>
 
-        <label htmlFor="RUT">RUT:</label>
-        <input type="number" {...register("rut", { required: true })} className="w-full border p-2 mb-2" />
+          <label htmlFor="RUT">RUT:</label>
+          <input type="number" {...register("rut", { required: true })} className="w-full border p-2 mb-2" />
 
-        <label htmlFor="nombreTutor">Nombre:</label>
-        <input type="text" {...register("nombreTutor", { required: true })} className="w-full border p-2 mb-2" />
+          <label htmlFor="nombreTutor">Nombre:</label>
+          <input type="text" {...register("nombreTutor", { required: true })} className="w-full border p-2 mb-2" />
 
-        <label htmlFor="correo">Correo:</label>
-        <input type="email" {...register("correo", { required: true })} className="w-full border p-2 mb-2" />
+          <label htmlFor="correo">Correo:</label>
+          <input type="email" {...register("correo", { required: true })} className="w-full border p-2 mb-2" />
 
-        <label htmlFor="celular">Celular:</label>
-        <input type="number" {...register("celular", { required: true })} className="w-full border p-2 mb-2" />
+          <label htmlFor="celular">Celular:</label>
+          <input type="number" {...register("celular", { required: true })} className="w-full border p-2 mb-2" />
 
-        <label htmlFor="direccion">Dirección:</label>
-        <input type="text" {...register("direccion", { required: true })} className="w-full border p-2 mb-2" />
+          <label htmlFor="direccion">Dirección:</label>
+          <input type="text" {...register("direccion", { required: true })} className="w-full border p-2 mb-2" />
+        </div>
+
+        <div>
+          <h2 className="text-xl font-bold mb-2">Datos mascota</h2>
+
+          <label htmlFor="rutChip">RUT o Chip:</label>
+          <input type="number" {...register("rutChip", { required: true })} className="w-full border p-2 mb-2" />
+
+          <label htmlFor="nombreMascota">Nombre:</label>
+          <input type="text" {...register("nombreMascota", { required: true })} className="w-full border p-2 mb-2" />
+
+          <label htmlFor="especie">Especie:</label>
+          <select {...register("especie", { required: true })} className="w-full border p-2 mb-2">
+            <option value="perro">Perro</option>
+            <option value="gato">Gato</option>
+            <option value="ave">Ave</option>
+            <option value="roedor">Roedor</option>
+            <option value="iguana">Iguana</option>
+          </select>
+
+          <label htmlFor="raza">Raza:</label>
+          <input type="text" {...register("raza")} className="w-full border p-2 mb-2" />
+        </div>
       </div>
 
-      <div>
-        <h2 className="text-xl font-bold mb-2">Datos mascota</h2>
-
-        <label htmlFor="rutChip">RUT o Chip:</label>
-        <input type="number" {...register("rutChip", { required: true })} className="w-full border p-2 mb-2" />
-
-        <label htmlFor="nombreMascota">Nombre:</label>
-        <input type="text" {...register("nombreMascota", { required: true })} className="w-full border p-2 mb-2" />
-
-        <label htmlFor="especie">Especie:</label>
-        <select {...register("especie", { required: true })} className="w-full border p-2 mb-2">
-          <option value="perro">Perro</option>
-          <option value="gato">Gato</option>
-          <option value="ave">Ave</option>
-          <option value="roedor">Roedor</option>
-          <option value="iguana">Iguana</option>
-        </select>
-
-        <label htmlFor="raza">Raza:</label>
-        <input type="text" {...register("raza")} className="w-full border p-2 mb-2" />
-
-        <label htmlFor="antecedentesMedicos">Antecedentes médicos:</label>
-        <textarea {...register("antecedentesMedicos", { required: true })} className="w-full border p-2 mb-2"></textarea>
+      <div className="mt-4">
+        <h2 className="text-xl font-bold mb-2">Síntomas/Observaciones</h2>
+        <textarea {...register("comentarios")} className="w-full border p-2 mb-2"></textarea>
       </div>
 
-      <button type="submit" className="col-span-2 bg-blue-500 text-white p-2 rounded">Registrar</button>
+      <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded mt-4">Registrar</button>
     </form>
   );
 }
