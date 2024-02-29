1ade0a55029d035760ec9e63873b60fbbb6e9f6a
gatunah <144285026+gatunah@users.noreply.github.com>
2024-02-29 18:09:22 -0300

    Formulario ficha
    se agregan algunos campos de la ficha de ingreso

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import IngresoPage from './pages/IngresoPage'
 import LoginPage from './pages/LoginPage'
+import FichaMedicaPage from './pages/FichaMedicaPage'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<IngresoPage/>}/>
       <Route path='/ing' element={<LoginPage/>}/>
+      <Route path='/fich' element={<FichaMedicaPage/>}/>
       </Routes>
     </BrowserRouter>
   )
--- client/src/pages/FichaMedicaPage.jsx
+++ client/src/pages/FichaMedicaPage.jsx
@@ -1,24 +1,63 @@
 import { useForm } from 'react-hook-form';
-import { registerRequest } from '../api/registroDatos';
+// import { registerRequest } from '../api/registroDatos';
 
 function FichaMedicaPage() {
-  const { register, handleSubmit } = useForm();
+  const { register, 
+    // handleSubmit 
+    } = useForm();
 
   return (
     <form
-      onSubmit={handleSubmit(async (values) => {
-        console.log(values);
-        const res = await registerRequest(values);
-        console.log(res);
-      })}>
+    //   onSubmit={handleSubmit(async (values) => {
+    //     console.log(values);
+    //     const res = await registerRequest(values);
+    //     console.log(res);
+    //   })}
+    >
       <div className="text-center mb-4">
         <h1 className="text-2xl font-bold">Ficha Médica</h1>
       </div>
+      <div htmlFor="">
+        <label htmlFor="RUT">RUT:</label>
+        <input
+          type="number"
+          {...register('rut', { required: true })}
+          className="w-full border p-2 mb-2"
+        />
+
+        <label htmlFor="nombreTutor">Nombre:</label>
+        <input
+          type="text"
+          {...register('nombreTutor', { required: true })}
+          className="w-full border p-2 mb-2"
+        />
+
+        <label htmlFor="correo">Correo:</label>
+        <input
+          type="email"
+          {...register('correo', { required: true })}
+          className="w-full border p-2 mb-2"
+        />
+
+        <label htmlFor="direccion">Dirección:</label>
+        <input
+          type="text"
+          {...register('direccion', { required: true })}
+          className="w-full border p-2 mb-2"
+        />
+        <hr></hr>
+
+        <label htmlFor="celular">Celular:</label>
+        <input
+          type="number"
+          {...register('celular', { required: true })}
+          className="w-full border p-2 mb-2"
+        />
+      </div>
+      <form></form>
 
       <section>
-        <div>
-            
-        </div>
+        <div></div>
       </section>
     </form>
   );
