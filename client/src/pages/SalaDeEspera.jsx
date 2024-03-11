61af29fbb076e6bc61005fe57420c57f0daabc2d
Jonathan Rojas <wafle48@gmail.com>
2024-03-11 12:29:15 -0300

    Styles Sala Espera

--- client/src/pages/SalaDeEspera.jsx
+++ client/src/pages/SalaDeEspera.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { filtroDatos } from '../api/salaEspera';
 import { useDatosM } from '../context/DatosMedicos';
 import { Sidebar } from '../components/Sidebar';
+import { Link } from 'react-router-dom';
 
 function SalaDeEspera() {
   const [datos, setDatos] = useState([]);
@@ -22,15 +23,15 @@ function SalaDeEspera() {
   }, [filtro]);
 
   return (
-    <div className="flex min-w-[calc(100vw-100px)] min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <div className="flex w-full h-screen">
       <Sidebar />
-      <div className="flex-grow p-4 text-center m-8">
-        <h1 className="text-2xl font-bold" id="tit-form-ing">
-          Sala de Espera
-        </h1>
-      </div>
-      <div className="bg-gray-200 p-4">
-        <div className="flex justify-end space-x-4">
+      <div className="flex flex-grow flex-col justify-start p-4 bg-gray-100">
+        <div className="text-center mb-8">
+          <h1 className="text-2xl font-bold" id="tit-form-ing">
+            Sala de Espera
+          </h1>
+        </div>
+        <div className="bg-gray-200 m-0 p-4 flex justify-end space-x-4">
           <button className="filter-btn" onClick={() => handleFiltro('espera')}>
             <img src="/img/reloj.png" alt="Icono en espera" />
           </button>
@@ -50,31 +51,36 @@ function SalaDeEspera() {
             <img src="/img/x.png" alt="Icono anulado" />
           </button>
         </div>
+        {/* Contenedor de las fichas */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-5">
+          {DatosM.Fichas &&
+            DatosM.Fichas.map((fila) => (
+              <Link
+                to={`/detalles/${fila._id}`}
+                key={fila._id}
+                className="card rounded-md p-2 hover:scale-125 duration-150 ml-6">
+                <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                  {/* Contenido de la ficha */}
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Nombre Paciente: {fila.ID_Mascota.Nombre}
+                  </h5>
+                  <h6 className="font-normal text-gray-700 dark:text-gray-400">
+                    Nombre Tutor: {fila.ID_Tutor.Nombre}
+                  </h6>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    ID: {fila._id}
+                  </p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Fecha: {hora(fila.Fecha)}
+                  </p>
+                  <p className="font-normal text-gray-700 dark:text-gray-400">
+                    Estado: {fila.Estado}
+                  </p>
+                </div>
+              </Link>
+            ))}
+        </div>
       </div>
-      {DatosM.Fichas &&
-        DatosM.Fichas.map((fila) => (
-          <div
-            key={fila._id}
-            className="card rounded-md mx-4 my-2 p-2 hover:scale-125 duration-150">
-            <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                Nombre Paciente: {fila.ID_Mascota.Nombre}
-              </h5>
-              <h6 className="font-normal text-gray-700 dark:text-gray-400">
-                Nombre Tutor: {fila.ID_Tutor.Nombre}
-              </h6>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                ID: {fila._id}
-              </p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Fecha: {hora(fila.Fecha)}
-              </p>
-              <p className="font-normal text-gray-700 dark:text-gray-400">
-                Estado: {fila.Estado}
-              </p>
-            </div>
-          </div>
-        ))}
     </div>
   );
 }
